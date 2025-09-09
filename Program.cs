d1b83e8034a22413b5bab953f05a73ed1885f62b
MatiasCaraballo <matias.caraballo.ez@gmail.com>
2025-09-09 13:34:22 -0300

    clean unnecesary lines from WeatherForecast

--- Program.cs
+++ Program.cs
@@ -134,11 +134,6 @@ if (app.Environment.IsDevelopment())
 app.UseAuthentication();
 app.UseAuthorization();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
 app.MapIdentityApi<IdentityUser>();
 
 app.MapControllers();
@@ -148,22 +143,6 @@ using (var scope = app.Services.CreateScope())
     var services = scope.ServiceProvider;
     await RoleInitializer.SeedRoles(services);
 }
-
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.RequireAuthorization("Admin");
 
 app.Run();
 
